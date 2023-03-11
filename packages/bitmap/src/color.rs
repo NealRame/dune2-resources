ba36214fbd738005aabe5bdb39b5924e370a1c4d
Julien Graziano <julien@graziano.fr>
2023-03-11 22:38:54 +0100

    refactor: palette

--- packages/bitmap/src/color.rs
+++ packages/bitmap/src/color.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use std::collections::HashMap;
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 pub struct Color {
     pub red: u8,
     pub green: u8,
@@ -8,5 +10,47 @@ pub struct Color {
 impl Color {
     pub fn new(red: u8, green: u8, blue: u8) -> Self {
         Self { red, green, blue }
+    }
+}
+
+pub struct Palette {
+    colors_index: HashMap<Color, usize>,
+    colors: Vec<Color>,
+}
+
+impl Palette {
+    pub fn new() -> Self {
+        Self {
+            colors_index: HashMap::new(),
+            colors: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, color: &Color) -> usize {
+        match self.colors_index.get(color) {
+            Some(index) => *index,
+            None => {
+                let index = self.colors.len();
+                self.colors_index.insert(*color, index);
+                self.colors.push(*color);
+                index
+            }
+        }
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = Color> + '_ {
+        self.colors.iter().copied()
+    }
+
+    pub fn color_index(&self, color: &Color) -> Option<usize> {
+        self.colors_index.get(&color).copied()
+    }
+
+    pub fn color_at(&self, index: usize) -> Color {
+        self.colors[index]
+    }
+
+    pub fn len(&self) -> usize {
+        self.colors.len()
     }
 }
