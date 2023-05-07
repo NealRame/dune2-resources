5576d55091f43bb69d8cca36a87ff484d00b92f0
Julien Graziano <julien@graziano.fr>
2023-05-07 21:56:38 +0200

    style: indent

--- packages/dune2/src/surface.rs
+++ packages/dune2/src/surface.rs
@@ -63,21 +63,23 @@ impl Surface {
 
     pub fn blit(
         &mut self,
-        bitmap: &Surface,
+        surface: &Surface,
         src_rect: Rect,
         dst_rect: Rect,
     ) -> &mut Self {
-        let src_rect = if let Some(rect) = src_rect.intersected(&bitmap.rect()) {
-            rect
-        } else {
-            Rect::zero()
-        };
+        let src_rect =
+            if let Some(rect) = src_rect.intersected(&surface.rect()) {
+                rect
+            } else {
+                Rect::zero()
+            };
 
-        let dst_rect = if let Some(rect) = dst_rect.intersected(&self.rect()) {
-            rect
-        } else {
-            Rect::zero()
-        };
+        let dst_rect =
+            if let Some(rect) = dst_rect.intersected(&self.rect()) {
+                rect
+            } else {
+                Rect::zero()
+            };
 
         let size = Size {
             width: min(src_rect.width(), dst_rect.width()),
@@ -88,7 +90,7 @@ impl Surface {
             Rect::from_point_and_size(src_rect.top_left(), size).iter(),
             Rect::from_point_and_size(dst_rect.top_left(), size).iter(),
         ).for_each(|(src, dst)| {
-            self.put_pixel(dst, bitmap.pixel(src));
+            self.put_pixel(dst, surface.pixel(src));
         });
 
         self
