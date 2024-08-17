a3aef62def9d781aa0c1e2fd4ed8024b496ce8d1
Julien Graziano <julien@graziano.fr>
2024-08-17 22:08:24 +0200

    refactor: update error reporting

--- packages/unpak/src/main.rs
+++ packages/unpak/src/main.rs
@@ -1,6 +1,8 @@
 mod command;
 mod config;
 
+use std::io::ErrorKind;
+
 use clap::Parser;
 use config::Cli;
 
@@ -8,7 +10,21 @@ fn main() {
     let config = Cli::parse();
 
     command::run(config).unwrap_or_else(|err| {
-        println!("Error: {}", err);
+        if err.is::<std::io::Error>() {
+            let io_err = err.downcast::<std::io::Error>().unwrap();
+
+            match io_err.kind() {
+                ErrorKind::AlreadyExists => {
+                    eprintln!("File '{}' already exists!", io_err.to_string());
+                    eprintln!("Use '-f' option to force file overwrite.");
+                },
+                _ => {
+                    eprintln!("IOError: {}", io_err);
+                }
+            }
+        } else {
+            println!("Error: {}", err);
+        }
         std::process::exit(1);
     });
 }
